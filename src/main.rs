dd8fc4b5b3eb874a846d1cfd3b16a6cae50a0ba8
sparkzky <sparkhhhhhhhhhh@outlook.com>
2025-04-07 11:40:43 +0800

    add setsid

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,19 @@ mod env;
 mod parser;
 
 use env::EnvManager;
+use libc::{ioctl, open, setsid, O_RDWR, TIOCSCTTY};
 use shell::Shell;
 
 fn main() {
+    // 1. setsid：成为新的 session leader
+    unsafe { setsid() };
+
+    // 2. 打开 /dev/tty（或你实现的等价路径）
+    let tty_path = std::ffi::CString::new("/dev/ttyS0").expect("CString::new failed");
+    let tty_fd = unsafe { open(tty_path.as_ptr(), O_RDWR) };
+
+    // 3. ioctl(fd, TIOCSCTTY)：设置为控制终端
+    unsafe { ioctl(tty_fd, TIOCSCTTY) };
     EnvManager::init();
     let mut shell = Shell::new();
     shell.exec();
